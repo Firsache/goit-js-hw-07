21336c729ada6cc96fbcbbbaa4dfbe29fa3ff190
Firsova Liubov <firsova.lyub@gmail.com>
2022-12-27 21:45:33 +0200

    task one

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+function createImageMarkUp(items) {
+    return items
+        .map(({ preview, original, description }) => {
+        return `
+            <div class="gallery__item">
+            <a class="gallery__link" href="large-image.jpg">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+            </div>
+        `
+    }).join('');
+}
+function renderImageMarkUp(items) {
+    galleryEl.innerHTML = createImageMarkUp(items);
+}
+renderImageMarkUp(galleryItems);
+
+galleryEl.addEventListener('click', onGalleryItemClickModalOpen);
+
+
+function onGalleryItemClickModalOpen(evt) {
+    evt.preventDefault();
+    let originalImg = evt.target.dataset.source;
+    let description = evt.target.alt;
+    console.log(description);
+
+    const instance = basicLightbox.create(`
+        <img src="${originalImg}" alt="${description}" width="800">
+    `).show()
+
+    document.addEventListener('keydown', evt => {
+        if (evt.code === 'Escape') {
+        instance.close()
+    }
+    });
+}
+
