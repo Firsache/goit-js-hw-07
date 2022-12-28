758c54c1567a5f074767fbd39aefcfc7c9edcdcd
Firsova Liubov <firsova.lyub@gmail.com>
2022-12-28 20:10:19 +0200

    added loading lazy

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -10,8 +10,9 @@ function createImageMarkUp(items) {
             <div class="gallery__item">
             <a class="gallery__link" href="large-image.jpg">
                 <img
-                class="gallery__image"
-                src="${preview}"
+                loading="lazy"
+                class="gallery__image lazyload"
+                data-src="${preview}"
                 data-source="${original}"
                 alt="${description}"
                 />
@@ -34,6 +35,9 @@ let instance = basicLightbox.create(`
 function onGalleryItemClickModalOpen(evt) {
     evt.preventDefault();
     
+    if (evt.target.nodeName !== 'IMG') {
+        return;
+    }
     instance.src = evt.target.dataset.source;
     // console.log(instance.src);
     
@@ -57,3 +61,26 @@ function closeModal() {
     instance.close()
     document.removeEventListener('keydown', onEscKeyBtnPress);
 }
+
+const addSrcAttributeToIm = () => {
+    const galleryImgEls = document.querySelectorAll('.gallery__image');
+
+    galleryImgEls.forEach(elem => {
+        elem.src = elem.dataset.src;
+    })
+}
+
+const createLazySizesScript = () => {
+    const script = document.createElement('.script');
+
+    script.src = 'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js';
+    return script;
+
+}
+
+if ('loading' in HTMLImageElement.prototype) {
+    addSrcAttributeToIm();
+} else {
+    document.body.append(createLazySizesScript())
+}
+    
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,37 +1,68 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const galleryEl = document.querySelector('.gallery');
+const galleryEl = document.querySelector(".gallery");
 
 function createImageMarkUp(items) {
-    return items
-        .map(({ preview, original, description }) => {
-        return `
+  return items
+    .map(({ preview, original, description }) => {
+      return `
             <a class="gallery__item" href="${original}">
-                <img class="gallery__image" src="${preview}" alt="${description}" />
+                <img loading="lazy"
+                class="gallery__image lazyload"
+                data-src="${preview}" 
+                alt="${description}" />
             </a>
-        `
-    }).join('');
+        `;
+    })
+    .join("");
 }
 function renderImageMarkUp(items) {
-    galleryEl.innerHTML = createImageMarkUp(items);
+  galleryEl.innerHTML = createImageMarkUp(items);
 }
 renderImageMarkUp(galleryItems);
 
-let lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250});
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
 
-galleryEl.addEventListener('click', onGalleryItemClickModalOpen);
-
+galleryEl.addEventListener("click", onGalleryItemClickModalOpen);
 
 function onGalleryItemClickModalOpen(evt) {
-    evt.preventDefault();
+  evt.preventDefault();
 
-    
-    lightbox.on(show.simplelightbox)
-    
-    document.addEventListener('keydown', evt => {
-        if (evt.code === 'Escape') {
-        close.simplelightbox
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+  lightbox.on(show.simplelightbox);
+
+  document.addEventListener("keydown", (evt) => {
+    if (evt.code === "Escape") {
+      close.simplelightbox;
     }
-    });
+  });
+}
+
+const addSrcAttributeToIm = () => {
+    const galleryImgEls = document.querySelectorAll('.gallery__image');
+
+    galleryImgEls.forEach(elem => {
+        elem.src = elem.dataset.src;
+    })
+}
+
+const createLazySizesScript = () => {
+    const script = document.createElement('.script');
+
+    script.src = 'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js';
+    return script;
+
+}
+
+if ('loading' in HTMLImageElement.prototype) {
+    addSrcAttributeToIm();
+} else {
+    document.body.append(createLazySizesScript())
 }
